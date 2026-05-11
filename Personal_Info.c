a50124858f26c4e2d4a221d3980832879def780f
atik8191 <atik07968191@gmail.com>
2026-05-11 12:24:25 +0600

    Add files via upload

---
+++ Personal_Info.c
@@ -0,0 +1,10 @@
+
+#include <stdio.h>
+
+int main() {
+    printf("Name   : Md Atikur Rahman\n");
+    printf("Address: Sirajganj Road,Salanga,Sirajganj\n");
+    printf("Student of CSE Depertment at Khawaja Younus Ali University\n");
+    printf("ID     : 07");
+    return 0;
+}
